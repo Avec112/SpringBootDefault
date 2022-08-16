a9100b41d201e6af62a9c50758ae189740583af0
Avec112 <avec112@gmail.com>
2022-08-16 10:12:59 +0200

    Initial Commit

---
+++ src/test/java/io/github/avec/SpringBootDefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.avec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDefaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
